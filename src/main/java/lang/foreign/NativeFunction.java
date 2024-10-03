53eebafb388c55a0375c91a1a9a70341429607d5
bellman <rndso15@naver.com>
2024-10-03 14:22:42 +0900

    java forign Logic 샘플 코드 추가
    Feat: java forign Logic 샘플 코드 추가

---
+++ src/main/java/lang/foreign/NativeFunction.java
@@ -0,0 +1,27 @@
+package lang.foreign;
+
+import java.lang.foreign.FunctionDescriptor;
+import java.lang.foreign.Linker;
+import java.lang.foreign.MemorySegment;
+import java.lang.foreign.SymbolLookup;
+import java.lang.foreign.ValueLayout;
+import java.lang.invoke.MethodHandle;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class NativeFunction {
+
+    @Test
+    void executeNativeFunction() throws Throwable {
+        Linker linker = Linker.nativeLinker();
+        SymbolLookup symbolLookup = linker.defaultLookup();
+        MemorySegment strlenMemorySegment = symbolLookup.find("strlen").orElseThrow();
+
+        FunctionDescriptor descriptor = FunctionDescriptor.of(ValueLayout.JAVA_LONG, ValueLayout.ADDRESS);
+        MethodHandle strlenMethodHandle = linker.downcallHandle(strlenMemorySegment, descriptor);
+
+        String line = "Hello";
+        long length = (long) strlenMethodHandle.invoke(line);// 0L is a null pointer
+        Assertions.assertEquals(length, line.length());
+    }
+}
