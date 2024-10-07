5292319ce79440894239dc681ee653bfb119c20e
bellman66 <rndso15@naver.com>
2024-10-07 21:14:33 +0900

    Invoke Getter Setter 샘플로직 추가
    Feat: Invoke Getter Setter 샘플로직 추가

--- src/main/java/lang/invoke/MethodHandleSample.java
+++ src/main/java/lang/invoke/MethodHandleSample.java
@@ -3,9 +3,15 @@ package lang.invoke;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
+
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MethodHandleSample {
+
+    // call field
+    public String message = "Initial Message";
 
     // call target method
     public static void printHelloWorld(String name) {
@@ -21,5 +27,31 @@ public class MethodHandleSample {
         MethodHandle methodHandle = lookup.findStatic(MethodHandleSample.class, "printHelloWorld", methodType);
 
         methodHandle.invokeExact("Youn");
+    }
+
+    @Test
+    public void testMethodHandleFieldGetter() throws Throwable {
+        // Lookup 객체 생성
+        MethodHandles.Lookup lookup = MethodHandles.lookup();
+
+        // 필드에 대한 MethodHandle 생성 (getter)
+        MethodHandle getter = lookup.findGetter(MethodHandleSample.class, "message", String.class);
+
+        String fieldValue = (String) getter.invoke(this);
+
+        assertEquals(fieldValue, "Initial Message");
+    }
+
+    @Test
+    public void testMethodHandleFieldSetter() throws Throwable {
+        // Lookup 객체 생성
+        MethodHandles.Lookup lookup = MethodHandles.lookup();
+
+        // 필드에 대한 MethodHandle 생성 (Setter)
+        MethodHandle setter = lookup.findSetter(MethodHandleSample.class, "message", String.class);
+
+        setter.invoke(this, "Changed Message");
+
+        assertEquals(message, "Changed Message");
     }
 }
