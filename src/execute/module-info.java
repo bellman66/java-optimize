c5d046573ba32857b673c5bb3def3892894e9543
bellman <rndso15@naver.com>
2024-10-09 18:29:40 +0900

    JPMS Module 매핑 및 테스트 로직 추가
    Feat: execute 모듈 생성 및 매핑
    Feat: 실행 로직 추가

---
+++ src/execute/module-info.java
@@ -0,0 +1,5 @@
+
+module optimize.execute {
+
+    requires external.util;
+}
---
+++ src/execute/src/Main.java
@@ -0,0 +1,12 @@
+package src;
+
+import com.util.Printer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+
+        printer.execute("Hello, World!");
+    }
+}
