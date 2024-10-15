79167f442b3c53bb7754a87f02b567c42d9ec56a
bellman66 <rndso15@naver.com>
2024-10-15 10:53:04 +0900

    Java 리플렉션 생성자 샘플 추가
    Feat: 리플렉션 생성자 샘플 추가

--- src/main/java/lang/reflect/ReflectectionSample.java
+++ src/main/java/lang/reflect/ReflectectionSample.java
@@ -2,13 +2,20 @@ package lang.reflect;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 public class ReflectectionSample {
 
     static class TestClass {
-        private String title = "default";
+        private String title;
+
+        public TestClass(String title) {
+            this.title = title;
+        }
 
         public void say(String message) {
             System.out.println(title + " say: " + message);
@@ -16,11 +23,22 @@ public class ReflectectionSample {
     }
 
     @Test
-    void reflectionTest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    void methodReflectionTest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         Class<?> targetClass = TestClass.class;
 
         Method method = targetClass.getDeclaredMethod("say", String.class);
 
-        method.invoke(new TestClass(), "Hello, World!");
+        method.invoke(new TestClass("jack"), "Hello, World!");
+    }
+
+    @Test
+    void constructorReflectionTest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException {
+        Class<?> targetClass = TestClass.class;
+
+        Constructor<?> constructor = targetClass.getConstructor(String.class);
+
+        TestClass testClass = (TestClass) constructor.newInstance("jack");
+
+        assertInstanceOf(TestClass.class, testClass);
     }
 }
