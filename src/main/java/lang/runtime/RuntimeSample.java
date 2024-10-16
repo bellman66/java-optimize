0982a380fe2f4d09388f2cd34601d79784c223c8
bellman <rndso15@naver.com>
2024-10-16 22:24:18 +0900

    Java Runtime command & gc Test
    Feat: Runtime command & gc Test

--- src/main/java/lang/runtime/RuntimeSample.java
+++ src/main/java/lang/runtime/RuntimeSample.java
@@ -19,4 +19,48 @@ public class RuntimeSample {
         System.out.println("totalMemory: " + totalMemory);
         System.out.println("maxMemory: " + maxMemory);
     }
+
+    @Test
+    void externalCommandTest() {
+        // given
+        Runtime runtime = Runtime.getRuntime();
+        String command = "notepad";
+
+        // when
+        try {
+            Process process = runtime.exec(command);
+            process.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void garbageCollectionTest() {
+        // when
+        Runtime runtime = Runtime.getRuntime();
+
+        // given
+        long freeMemory = runtime.freeMemory();
+        long totalMemory = runtime.totalMemory();
+        long maxMemory = runtime.maxMemory();
+
+        // then
+        System.out.println("freeMemory: " + freeMemory);
+        System.out.println("totalMemory: " + totalMemory);
+        System.out.println("maxMemory: " + maxMemory);
+
+        // when
+        runtime.gc();
+
+        // given
+        freeMemory = runtime.freeMemory();
+        totalMemory = runtime.totalMemory();
+        maxMemory = runtime.maxMemory();
+
+        // then
+        System.out.println("freeMemory: " + freeMemory);
+        System.out.println("totalMemory: " + totalMemory);
+        System.out.println("maxMemory: " + maxMemory);
+    }
 }
