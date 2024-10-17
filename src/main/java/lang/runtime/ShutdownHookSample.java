45de814caf144619f797f1573b46b40752e2beff
bellman <rndso15@naver.com>
2024-10-17 23:10:35 +0900

    Java Halt Sample 추가
    Feat: Java Halt Sample 추가

--- src/main/java/lang/runtime/ShutdownHookSample.java
+++ src/main/java/lang/runtime/ShutdownHookSample.java
@@ -22,4 +22,27 @@ public class ShutdownHookSample {
         System.out.println("Exiting program.");
         runtime.exit(0);
     }
+
+    /**
+     * halt() 메소드는 JVM을 즉시 종료시키는 메소드이다.
+     * ShutdownHook 로직이 아니기 때문에 print문이 출력되지 않는다.
+     **/
+    @Test
+    void haltTest() throws InterruptedException {
+        // given
+        Runtime runtime = Runtime.getRuntime();
+
+        runtime.addShutdownHook(new Thread(() -> {
+            System.out.println("Hook : JVM is shutting down.");
+        }));
+
+        // when
+        System.out.println("Program is running...");
+
+        Thread.sleep(3000);
+
+        // then
+        System.out.println("Exiting program.");
+        runtime.halt(0);
+    }
 }
