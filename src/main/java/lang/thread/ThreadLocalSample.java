61243dd8ffe21d63e2be95ed2b0e5278f282ea1b
bellman66 <rndso15@naver.com>
2024-10-21 09:54:26 +0900

    Java Ping Process Test 추가
    Feat: Java Ping Process Test 추가

--- src/main/java/lang/thread/ThreadLocalSample.java
+++ src/main/java/lang/thread/ThreadLocalSample.java
@@ -1,2 +1,42 @@
-package lang.thread;public class ThreadLocalSample {
+package lang.thread;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class ThreadLocalSample {
+
+    static class TaskContext {
+
+        private static final ThreadLocal<String> THREAD_LOCAL = new ThreadLocal<>();
+
+        public static void set(String value) {
+            THREAD_LOCAL.set(value);
+        }
+
+        public static String get() {
+            return THREAD_LOCAL.get();
+        }
+    }
+
+    @Test
+    void ThreadLocalTest() {
+        // given & when
+        Thread thread1 = new Thread(() -> {
+            assertNull(TaskContext.get());
+            TaskContext.set("thread1");
+            assertEquals("thread1", TaskContext.get());
+        });
+
+        Thread thread2 = new Thread(() -> {
+            assertNull(TaskContext.get());
+            TaskContext.set("thread2");
+            assertEquals("thread2", TaskContext.get());
+        });
+
+        // then
+        thread1.start();
+        thread2.start();
+    }
 }
