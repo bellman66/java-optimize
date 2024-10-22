33f1c348c1855e1fad269cf31976cebc9dd2e720
bellman <rndso15@naver.com>
2024-10-22 23:49:44 +0900

    Java nio channel 샘플 추가
    Feat: Java nio channel 샘플 추가

---
+++ src/main/java/nio/ChannelSample.java
@@ -0,0 +1,36 @@
+package nio;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import org.junit.jupiter.api.Test;
+
+public class ChannelSample {
+
+    @Test
+    void channelTest() {
+        // given
+        Path path = Path.of(System.getProperty("user.dir"), "src", "main", "java", "nio", "example.txt");
+
+        // when
+        try (
+                FileChannel fileChannel = FileChannel.open(path, StandardOpenOption.READ)
+        ) {
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+            while (fileChannel.read(buffer) != -1) {
+                buffer.flip();
+
+                while (buffer.hasRemaining()) {
+                    System.out.print((char) buffer.get());
+                }
+
+                buffer.clear();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
