13cd6b7a6477ab13242ec431664c60cb253f8641
bellman66 <rndso15@naver.com>
2024-10-23 21:19:21 +0900

    Java Ping Process Test 추가
    Feat: Java Ping Process Test 추가

--- src/main/java/lang/process/ProcessSample.java
+++ src/main/java/lang/process/ProcessSample.java
@@ -1,13 +1,18 @@
 package lang.process;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class ProcessSample {
 
     @Test
     void processTest() {
         // given
-        String command =  "ls -al";
+        String command = "ls -al";
 
         try {
             Process process = Runtime.getRuntime().exec(command);
@@ -15,6 +20,32 @@ public class ProcessSample {
             process.getInputStream().transferTo(System.out);
 
             process.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void processBuilderTest() {
+        // given
+        ProcessBuilder processBuilder = new ProcessBuilder(Arrays.asList("ping", "-c", "3", "www.google.com"));
+
+        try {
+            Process process = processBuilder.start();
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line).append("\n");
+            }
+
+            int exit = process.waitFor();
+
+            System.out.println(stringBuilder);
+            Assertions.assertNotEquals(stringBuilder.length(), 0);
+            Assertions.assertEquals(exit, 0);
         } catch (Exception e) {
             e.printStackTrace();
         }
