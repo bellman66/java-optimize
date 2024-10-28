3dc8115776adec560472517be14c3119f22cd9b8
bellman <rndso15@naver.com>
2024-10-28 23:26:38 +0900

    Java Static Sync 로직 추가
    Feat: Java Static Sync 로직 추가

---
+++ src/main/java/keyword/StaticSync.java
@@ -0,0 +1,37 @@
+package keyword;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.junit.jupiter.api.Test;
+
+public class StaticSync {
+
+    private static int count = 0;
+
+    // Static method -> class level lock
+    synchronized static void syncMethod() {
+        count += 1;
+        System.out.println("Thread : " + Thread.currentThread().getName() + " count : " + count);
+    }
+
+    @Test
+    void staticSyncMethodTest() {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        try {
+            executorService.submit(() -> {
+                for (int i = 0; i < 10; i++) {
+                    syncMethod();
+                }
+            });
+
+            executorService.submit(() -> {
+                for (int i = 0; i < 10; i++) {
+                    syncMethod();
+                }
+            });
+        } finally {
+            executorService.shutdown();
+        }
+    }
+}
