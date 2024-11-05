b4f655fd58124a94f09deff7e312f875d9ea16f0
bellman66 <rndso15@naver.com>
2024-11-05 10:03:40 +0900

    Feat: format 재업로드

--- src/main/java/text/FormatSample.java
+++ src/main/java/text/FormatSample.java
@@ -1,2 +1,36 @@
-package text;public class FormatSample {
+package text;
+
+import org.junit.jupiter.api.Test;
+
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FormatSample {
+
+    @Test
+    void DateFormat() {
+        // given
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        // when
+        String date = sdf.format(System.currentTimeMillis());
+
+        // then
+        assertNotNull(date);
+    }
+
+    @Test
+    void NumberFormat() {
+        // given
+        NumberFormat format = NumberFormat.getInstance();
+
+        // when
+        String number = format.format(1234567890);
+
+        // then
+        assertEquals("1,234,567,890", number);
+    }
+
 }
