874357a0291153908eda8848c8c85d85692db04b
bellman <rndso15@naver.com>
2024-11-18 22:03:53 +0900

    feat: unsafe Sample

--- src/main/java/internal/unsafe/UnsafeSample.java
+++ src/main/java/internal/unsafe/UnsafeSample.java
@@ -1,4 +1,26 @@
 package internal.unsafe;
 
+import java.lang.reflect.Field;
+import org.junit.jupiter.api.Test;
+import sun.misc.Unsafe;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class UnsafeSample {
+
+    @Test
+    void internalAccess() throws NoSuchFieldException, IllegalAccessException, InstantiationException {
+        // given
+        Field field = Unsafe.class.getDeclaredField("theUnsafe");
+        field.setAccessible(true);
+        Unsafe unsafe = (Unsafe) field.get(null);
+
+        // when
+        long address = unsafe.allocateMemory(4L);
+        unsafe.putInt(address, 123);
+        int result = unsafe.getInt(address);
+
+        // then
+        assertEquals(123, result);
+    }
 }
