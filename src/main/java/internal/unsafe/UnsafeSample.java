c637cecef0d30a88e813f5d6a8ba919b3d8546ea
bellman <rndso15@naver.com>
2024-11-19 22:07:49 +0900

    feat: unsafe instance Sample

--- src/main/java/internal/unsafe/UnsafeSample.java
+++ src/main/java/internal/unsafe/UnsafeSample.java
@@ -1,12 +1,25 @@
 package internal.unsafe;
 
 import java.lang.reflect.Field;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import sun.misc.Unsafe;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UnsafeSample {
+
+    class Dummy {
+        private String value = "default";
+
+        public Dummy() {
+            this.value = "init";
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
 
     @Test
     void internalAccess() throws NoSuchFieldException, IllegalAccessException, InstantiationException {
@@ -22,5 +35,19 @@ public class UnsafeSample {
 
         // then
         assertEquals(123, result);
+    }
+
+    @Test
+    void instanceAccess() throws NoSuchFieldException, IllegalAccessException, InstantiationException {
+        // given
+        Field field = Unsafe.class.getDeclaredField("theUnsafe");
+        field.setAccessible(true);
+        Unsafe unsafe = (Unsafe) field.get(null);
+
+        // when
+        Dummy dummy = (Dummy) unsafe.allocateInstance(Dummy.class);
+
+        // then
+        Assertions.assertNull(dummy.getValue());
     }
 }
