3c1ced010ac42f239ad855881cb65b8b8b61936d
bellman <rndso15@naver.com>
2024-11-20 22:33:15 +0900

    Java Thread Executor Async 샘플 추가
    Feat: Java Thread Executor Async 샘플 추가

--- src/main/java/util/concurrent/executor/SingletonExecutorSample.java
+++ src/main/java/util/concurrent/executor/SingletonExecutorSample.java
@@ -1,5 +1,6 @@
 package util.concurrent.executor;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import org.junit.jupiter.api.Test;
@@ -28,5 +29,26 @@ public class SingletonExecutorSample {
         while (!executorService.isTerminated()) {
             Thread.sleep(1000);
         }
+    }
+
+    @Test
+    void singletonExecutorAsyncSample() {
+        // given
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        // when
+        CompletableFuture<Void> completableFuture = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return null;
+        }, executorService).thenAccept(result -> {
+            System.out.println("Thread Name: " + Thread.currentThread().getName());
+        });
+
+        // then
+        completableFuture.join();
     }
 }
