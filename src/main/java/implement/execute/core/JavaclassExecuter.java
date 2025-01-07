03dc5006c7eb9f8d8b71919b2542b209f8dd2ffd
bellman66 <rndso15@naver.com>
2025-01-07 21:12:37 +0900

    Feat: add ClassExecuter

---
+++ src/main/java/implement/execute/core/JavaclassExecuter.java
@@ -0,0 +1,19 @@
+package implement.execute.core;
+
+public class JavaclassExecuter {
+
+    public static String execute(byte[] classByte) {
+        HackSystem.clearBuffer();
+        ClassModifier cm = new ClassModifier(classByte);
+        byte[] modiBytes = cm.modifyUTF8Constant("java/lang/System", "implement/execute/core/HackSystem");
+        HotSwapClassLoader loader = new HotSwapClassLoader();
+
+        Class<?> clazz = loader.loadByte(modiBytes);
+        try {
+            clazz.getMethod("main", new Class[] {String[].class}).invoke(null, new String[] {null});
+        } catch (Exception e) {
+            e.printStackTrace(HackSystem.out);
+        }
+        return HackSystem.getBufferString();
+    }
+}
